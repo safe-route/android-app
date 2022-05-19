b2eed195b7b7f481532b8324136e85732b956efb
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-05-19 11:51:06 +0700

    setup retrofit

---
+++ app/src/main/java/com/c22ps305team/saferoute/api/ApiConfig.kt
@@ -0,0 +1,31 @@
+package com.c22ps305team.saferoute.api
+
+import com.c22ps305team.saferoute.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiConfig {
+
+    companion object {
+        fun getApiService(): ApiService {
+            val loggingInterceptor = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
+
+            val client = OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+            val retrofit =
+                Retrofit.Builder()
+                    .baseUrl("")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/c22ps305team/saferoute/api/ApiService.kt
@@ -0,0 +1,7 @@
+package com.c22ps305team.saferoute.api
+
+interface ApiService {
+
+//    Api Endpoint will be here
+
+}
