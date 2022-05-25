7eefec1b90573d0d07d599f494ee88511427e82d
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-05-25 12:41:55 +0700

    refactor code. make utils function vectorToBitmap

--- app/src/main/java/com/c22ps305team/saferoute/ui/main/location/MapsFragment.kt
+++ app/src/main/java/com/c22ps305team/saferoute/ui/main/location/MapsFragment.kt
@@ -1,27 +1,24 @@
 package com.c22ps305team.saferoute.ui.main.location
 
-import android.graphics.Bitmap
-import android.graphics.Canvas
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
-import androidx.annotation.DrawableRes
-import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.c22ps305team.saferoute.R
 import com.c22ps305team.saferoute.data.ClusteringDataModel
 import com.c22ps305team.saferoute.databinding.FragmentMapsBinding
 import com.c22ps305team.saferoute.utils.readJsonFile
+import com.c22ps305team.saferoute.utils.vectorToBitmap
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.*
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.gson.Gson
 
 class MapsFragment : Fragment() {
@@ -84,23 +81,6 @@ class MapsFragment : Fragment() {
             true
         }
 
-    }
-
-    private fun vectorToBitmap(@DrawableRes id: Int): BitmapDescriptor {
-        val vectorDrawable = ResourcesCompat.getDrawable(resources, id, null)
-        if (vectorDrawable == null) {
-            Log.e("BitmapHelper", "Resource not found")
-            return BitmapDescriptorFactory.defaultMarker()
-        }
-        val bitmap = Bitmap.createBitmap(
-            vectorDrawable.intrinsicWidth,
-            vectorDrawable.intrinsicHeight,
-            Bitmap.Config.ARGB_8888
-        )
-        val canvas = Canvas(bitmap)
-        vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
-        vectorDrawable.draw(canvas)
-        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 
     override fun onDestroy() {
