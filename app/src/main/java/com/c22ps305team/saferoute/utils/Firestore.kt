8f12823ffcdb59b60765beef2d3e80aa6d642e52
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-05-30 14:21:34 +0700

    setup firebase SDK

---
+++ app/src/main/java/com/c22ps305team/saferoute/utils/Firestore.kt
@@ -0,0 +1,7 @@
+package com.c22ps305team.saferoute.utils
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class Firestore {
+    val firestoreDatabse = FirebaseFirestore.getInstance()
+}
