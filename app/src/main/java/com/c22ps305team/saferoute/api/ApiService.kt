c008e03c0e9f117e946f657e2ebb79f4248b7105
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-05-31 19:49:02 +0700

    make interface to hit maps direction API endpoint

--- app/src/main/java/com/c22ps305team/saferoute/api/ApiService.kt
+++ app/src/main/java/com/c22ps305team/saferoute/api/ApiService.kt
@@ -1,7 +1,17 @@
 package com.c22ps305team.saferoute.api
 
+import com.c22ps305team.saferoute.data.DirectionsResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
 
-//    Api Endpoint will be here
+    @GET("json")
+    fun getDirection(
+        @Query("origin") origin: String,
+        @Query("destination") destination: String,
+        @Query("key") key: String,
+    ): Call<DirectionsResponse>
 
 }
