49ead2ac84076cbdabbbaa2f6a4ccf1ee57b5ea1
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-06-01 18:15:59 +0700

    make interface to hit maps search place API endpoint

--- app/src/main/java/com/c22ps305team/saferoute/api/ApiService.kt
+++ app/src/main/java/com/c22ps305team/saferoute/api/ApiService.kt
@@ -1,6 +1,7 @@
 package com.c22ps305team.saferoute.api
 
 import com.c22ps305team.saferoute.data.DirectionsResponse
+import com.c22ps305team.saferoute.data.SearchPlaceResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -13,5 +14,11 @@ interface ApiService {
         @Query("destination") destination: String,
         @Query("key") key: String,
     ): Call<DirectionsResponse>
+
+    @GET("place/textsearch/json")
+    fun searchPlace(
+        @Query("query") query: String,
+        @Query("key") key: String
+    ): Call<SearchPlaceResponse>
 
 }
