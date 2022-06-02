b6c008f0ed1be0496248e5556ccf81eea8be6e0d
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-06-02 20:48:54 +0700

    fix conflict merge

--- app/src/main/java/com/c22ps305team/saferoute/adapter/ListPlaceAdapter.kt
+++ app/src/main/java/com/c22ps305team/saferoute/adapter/ListPlaceAdapter.kt
@@ -1,5 +1,6 @@
 package com.c22ps305team.saferoute.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.c22ps305team.saferoute.data.ResultsItem
@@ -9,12 +10,14 @@ class ListPlaceAdapter(private val listPlace: List<ResultsItem>) :
     RecyclerView.Adapter<ListPlaceAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListPlaceAdapter.ViewHolder {
-        TODO("Inflate the item view")
+        val binding = ItemPlaceBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
 
     }
 
     override fun onBindViewHolder(holder: ListPlaceAdapter.ViewHolder, position: Int) {
-        TODO("Bind data to View")
+        holder.binding.tvPlaceName.text = listPlace[position].name
+        holder.binding.tvPlaceDetail.text = listPlace[position].formattedAddress
     }
 
     override fun getItemCount(): Int = listPlace.size
