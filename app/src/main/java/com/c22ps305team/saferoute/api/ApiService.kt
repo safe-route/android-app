ac827918e6a943548bbb26ada2c7bc4934537642
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-06-10 10:33:09 +0700

    limit place search

--- app/src/main/java/com/c22ps305team/saferoute/api/ApiService.kt
+++ app/src/main/java/com/c22ps305team/saferoute/api/ApiService.kt
@@ -19,6 +19,7 @@ interface ApiService {
 
     @GET("place/textsearch/json")
     fun searchPlace(
+        @Query("location") location: String,
         @Query("query") query: String,
         @Query("language") language: String,
         @Query("key") key: String
