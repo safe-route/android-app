3e579a9ab718e3503b126d940d8087e5cece372b
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-06-11 22:36:29 +0700

    add function to get data area statistic

--- app/src/main/java/com/c22ps305team/saferoute/ui/main/location/MapsViewModel.kt
+++ app/src/main/java/com/c22ps305team/saferoute/ui/main/location/MapsViewModel.kt
@@ -1,13 +1,46 @@
 package com.c22ps305team.saferoute.ui.main.location
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.c22ps305team.saferoute.api.ApiAreaStatisticConfig
+import com.c22ps305team.saferoute.data.AreaStatisticResponse
+import com.c22ps305team.saferoute.data.CrimeInfos
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MapsViewModel: ViewModel() {
+class MapsViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is maps Fragment"
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _dataAreaStatistic = MutableLiveData<AreaStatisticResponse>()
+    val dataAreaStatistic: LiveData<AreaStatisticResponse> = _dataAreaStatistic
+
+
+    fun getDataAreaStatistic(areaName: JsonObject) {
+        _isLoading.value = true
+        val client = ApiAreaStatisticConfig.getApiService().getAreaStatistic(areaName)
+        client.enqueue(object : Callback<AreaStatisticResponse> {
+            override fun onResponse(
+                call: Call<AreaStatisticResponse>,
+                response: Response<AreaStatisticResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful && response.body() != null) {
+                    _dataAreaStatistic.value = response.body()
+                }
+            }
+
+            override fun onFailure(call: Call<AreaStatisticResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e("Error Gan: ", "onFailure: ${t.message}")
+            }
+
+        })
     }
-    val text: LiveData<String> = _text
+
 }
