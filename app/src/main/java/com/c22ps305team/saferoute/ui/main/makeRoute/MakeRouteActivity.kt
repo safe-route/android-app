c2da33e7481477cfe2672bf92ec69895978c9d35
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-06-12 18:42:53 +0700

    make location update

--- app/src/main/java/com/c22ps305team/saferoute/ui/main/makeRoute/MakeRouteActivity.kt
+++ app/src/main/java/com/c22ps305team/saferoute/ui/main/makeRoute/MakeRouteActivity.kt
@@ -1,14 +1,19 @@
 package com.c22ps305team.saferoute.ui.main.makeRoute
 
 import android.Manifest
+import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.RelativeLayout
 import android.widget.Toast
+import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -24,18 +29,19 @@ import com.c22ps305team.saferoute.adapter.ListPlaceAdapter
 import com.c22ps305team.saferoute.data.DirectionsResponse
 import com.c22ps305team.saferoute.data.ResultsItem
 import com.c22ps305team.saferoute.databinding.ActivityMakeRouteBinding
+import com.c22ps305team.saferoute.utils.decodePolyLines
 import com.c22ps305team.saferoute.utils.hideKeyboard
 import com.facebook.shimmer.ShimmerFrameLayout
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.maps.android.PolyUtil
 
 
 class MakeRouteActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -45,8 +51,13 @@ class MakeRouteActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMakeRouteBinding
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var userCurrentLocation: LatLng
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private var userCurrentLocation: LatLng? = null
     private val makeRouteViewModel by viewModels<MakeRouteViewModel>()
+
+    private var isTracking = false
+    private var boundsBuilder = LatLngBounds.Builder()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,6 +76,121 @@ class MakeRouteActivity : AppCompatActivity(), OnMapReadyCallback {
         }
         setupBottomSheetBehavior()
         observeViewModel()
+    }
+
+    // Map Callback
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.uiSettings.isMyLocationButtonEnabled = false
+
+        val jakarta = LatLng(-6.229728, 106.6894283)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jakarta, 10f))
+
+        binding.btnGetMyLocation.setOnClickListener {
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userCurrentLocation!!, 17F))
+        }
+
+        getMyLocation()
+        createLocationRequest()
+        createLocationCallBack()
+
+        binding.btnStart.setOnClickListener {
+            if (!isTracking) {
+                updateTrackingStatus(true)
+                startLocationUpdates()
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userCurrentLocation!!, 17F))
+            } else {
+                updateTrackingStatus(false)
+                stopLocationUpdates()
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(14F))
+            }
+        }
+    }
+
+    private val resolutionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.StartIntentSenderForResult()
+        ) { result ->
+            when (result.resultCode) {
+                RESULT_OK ->
+                    Log.i(
+                        MakeRouteActivity::class.java.simpleName,
+                        "onActivityResult: All location settings are satisfied."
+                    )
+                RESULT_CANCELED ->
+                    Toast.makeText(
+                        this@MakeRouteActivity,
+                        "Anda harus mengaktifkan GPS untuk menggunakan aplikasi ini!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+            }
+        }
+
+    private fun updateTrackingStatus(newStatus: Boolean) {
+        isTracking = newStatus
+        if (isTracking) {
+            binding.btnStart.text = "Stop"
+        } else {
+            binding.btnStart.text = getString(R.string.start_journey)
+        }
+    }
+
+    fun createLocationRequest() {
+        locationRequest = LocationRequest.create()
+        locationRequest.interval = 500
+        locationRequest.fastestInterval = 500
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+        val client = LocationServices.getSettingsClient(this)
+        client.checkLocationSettings(builder.build())
+            .addOnSuccessListener { getMyLocation() }.addOnFailureListener { exception ->
+                if (exception is ResolvableApiException) {
+                    try {
+                        resolutionLauncher.launch(
+                            IntentSenderRequest.Builder(exception.resolution).build()
+                        )
+                    } catch (sendEx: IntentSender.SendIntentException) {
+                        Toast.makeText(this@MakeRouteActivity, sendEx.message, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }
+    }
+
+    private fun createLocationCallBack() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                for (location in locationResult.locations) {
+                    val lastLatLng = LatLng(location.latitude, location.longitude)
+                    Log.d(
+                        MakeRouteActivity::class.java.simpleName,
+                        "onLocationResult: " + location.latitude + ", " + location.longitude
+                    )
+
+                    boundsBuilder.include(lastLatLng)
+                    val bounds: LatLngBounds = boundsBuilder.build()
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 500))
+
+                }
+            }
+        }
+    }
+
+    private fun startLocationUpdates() {
+        try {
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        } catch (exception: SecurityException) {
+            Log.e(MakeRouteActivity::class.java.simpleName, "Error : " + exception.message)
+        }
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
     // Observe Viewmodel
@@ -87,24 +213,8 @@ class MakeRouteActivity : AppCompatActivity(), OnMapReadyCallback {
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
 
         val polyLine = geoCodeWayPoint?.routes?.get(0)?.overviewPolyline?.points
-        val routeDecode = PolyUtil.decode(polyLine)
-
-        mMap.addPolyline(PolylineOptions().addAll(routeDecode).color(Color.GREEN).width(15F))
-    }
-
-    // Map Callback
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        mMap.uiSettings.isMyLocationButtonEnabled = false
-
-        val jakarta = LatLng(-6.229728, 106.6894283)
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jakarta, 10f))
-
-        binding.btnGetMyLocation.setOnClickListener {
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userCurrentLocation, 17F))
-        }
-
-        getMyLocation()
+        val routeDecode = decodePolyLines(polyLine.toString())
+        mMap.addPolyline(PolylineOptions().addAll(routeDecode!!).color(Color.GREEN).width(15F))
     }
 
     // Bottom Sheet Behavior
@@ -135,8 +245,8 @@ class MakeRouteActivity : AppCompatActivity(), OnMapReadyCallback {
         btnPickCurrentLocation.setOnClickListener {
             geocoder = Geocoder(this)
             val address = geocoder.getFromLocation(
-                userCurrentLocation.latitude,
-                userCurrentLocation.longitude,
+                userCurrentLocation?.latitude!!,
+                userCurrentLocation?.longitude!!,
                 1
             )
             val streetName = address[0].getAddressLine(0).split(",")
@@ -169,14 +279,18 @@ class MakeRouteActivity : AppCompatActivity(), OnMapReadyCallback {
     // Get user location
     private fun getMyLocation() {
         mMap.isMyLocationEnabled = true
-
         if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION) &&
             checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
         ) {
             fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
                 if (location != null) {
                     userCurrentLocation = LatLng(location.latitude, location.longitude)
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userCurrentLocation, 18F))
+                    mMap.animateCamera(
+                        CameraUpdateFactory.newLatLngZoom(
+                            userCurrentLocation!!,
+                            18F
+                        )
+                    )
                 } else {
                     Toast.makeText(
                         this@MakeRouteActivity,
@@ -234,17 +348,28 @@ class MakeRouteActivity : AppCompatActivity(), OnMapReadyCallback {
 
         rvListPlace.layoutManager = LinearLayoutManager(this)
         val adapter = ListPlaceAdapter(listPlace) {
+            val wrapperBtnControll = findViewById<RelativeLayout>(R.id.wrapperBtnControll)
+            val wrappertBottomSheet = findViewById<ConstraintLayout>(R.id.bottomSheetWrapper)
+            val wrapperBtnStart = findViewById<RelativeLayout>(R.id.wrapperBtnStart)
+            val bottomSheet = findViewById<ConstraintLayout>(R.id.bottomSheetMakeRoute)
+
+            wrapperBtnControll.visibility = View.GONE
+            wrappertBottomSheet.visibility = View.GONE
+            bottomSheet.visibility = View.GONE
+            wrapperBtnStart.visibility = View.VISIBLE
+
+            mMap.clear()
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(13F))
             makeRoute(it)
         }
         rvListPlace.adapter = adapter
-
     }
 
     // Make routing
-    private fun makeRoute(listPlace: List<ResultsItem>) {
-        val geoMetry = listPlace[0].geometry
+    private fun makeRoute(listPlace: ResultsItem) {
+        val geoMetry = listPlace.geometry
         val destinationPlace = "${geoMetry?.location?.lat},${geoMetry?.location?.lng}"
-        val originPlace = "${userCurrentLocation.latitude}, ${userCurrentLocation.longitude}"
+        val originPlace = "${userCurrentLocation?.latitude}, ${userCurrentLocation?.longitude}"
         makeRouteViewModel.getDirection(originPlace, destinationPlace, BuildConfig.API_KEY)
     }
 
@@ -268,6 +393,9 @@ class MakeRouteActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onResume()
 
         shimerFrameLayout.stopShimmer()
+        if (isTracking) {
+            startLocationUpdates()
+        }
     }
 
     override fun onPause() {
@@ -275,6 +403,7 @@ class MakeRouteActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onPause()
 
         shimerFrameLayout.stopShimmer()
+        stopLocationUpdates()
     }
 
 }
