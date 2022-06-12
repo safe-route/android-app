8cd9816924cc4f309a0859ece6c200fefbea0d82
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-06-12 19:00:40 +0700

    make service to track habit user

--- app/src/main/java/com/c22ps305team/saferoute/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/c22ps305team/saferoute/ui/main/home/HomeFragment.kt
@@ -1,30 +1,40 @@
 package com.c22ps305team.saferoute.ui.main.home
 
 import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
-import android.graphics.Color
 import android.location.Geocoder
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.c22ps305team.saferoute.data.InfoTips
-import com.c22ps305team.saferoute.data.InfoTipsData
 import com.c22ps305team.saferoute.data.Statistic
+import com.c22ps305team.saferoute.database.dataPredict.DataPredict
+import com.c22ps305team.saferoute.database.dataTraining.DataTraining
 import com.c22ps305team.saferoute.databinding.FragmentHomeBinding
 import com.c22ps305team.saferoute.ui.main.detail.DetailPlaceActivity
-import com.c22ps305team.saferoute.ui.main.detail.DetailTipsActivity
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.c22ps305team.saferoute.ui.main.makeRoute.MakeRouteActivity
+import com.c22ps305team.saferoute.utils.DateUtils
+import com.c22ps305team.saferoute.utils.RoomViewModelFactory
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.firebase.firestore.*
 import com.google.firebase.firestore.EventListener
-import com.google.type.LatLng
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import kotlinx.coroutines.*
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -34,53 +44,102 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var homeViewModel: HomeViewModel
-
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
     private lateinit var placeInfoData: ArrayList<Statistic>
     private lateinit var placeInfoAdapter: PlaceInfoAdapter
     private lateinit var db: FirebaseFirestore
 
+
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
-
-    private lateinit var tipsInfoAdapter: TipsInfoAdapter
-    private lateinit var listInfoTips: ArrayList<InfoTips>
-
-    //private lateinit var cityName: String
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
 
+        homeViewModel = obtainViewModel(requireActivity())
+        homeViewModel.createModel("userTest")
 
-        /*setupViewModel()
-        setupObserver()*/
 
-
+        doStreamDataTraining()
         getCurrentLocation()
-
-
+        createLocationRequest()
+        createLocationCallBack()
+        startLocationUpdates()
 
         setupPlaceInfo()
-        setupTipsInfo()
+    }
 
-        setupBanner()
-
-
+    private fun doStreamDataTraining() {
+        var data = JsonArray()
+        var dataTraining = JsonObject()
+        homeViewModel.getAllDataTraining().observe(viewLifecycleOwner) { listData ->
+            if (listData.size >= 720) {
+                val obj = JsonObject()
+                for (list in listData) {
+                    obj.addProperty("latitude", list.latitude)
+                    obj.addProperty("longitude", list.longitude)
+                    obj.addProperty("datetime", list.date)
+                    data.add(obj)
+                }
+                dataTraining.add("data", data)
+                homeViewModel.trainModel("userTest", dataTraining)
+                doStreamDataPredict()
+                lifecycleScope.launch {
+                    delay(2000)
+                    data = JsonArray()
+                    dataTraining = JsonObject()
+                }
+                homeViewModel.deleteDataTraining()
+            }
+        }
     }
 
 
+    private fun doStreamDataPredict() {
+        var data = JsonArray()
+        var dataPredict = JsonObject()
+        homeViewModel.getAllData().observe(viewLifecycleOwner) { listData ->
+            if (listData.size >= 15) {
+                val obj = JsonObject()
+                for (list in listData) {
+                    obj.addProperty("latitude", list.latitude)
+                    obj.addProperty("longitude", list.longitude)
+                    obj.addProperty("datetime", list.date)
+                    data.add(obj)
+                }
+                dataPredict.addProperty("email", "fillahfirdausyah22@gmail.com")
+                dataPredict.addProperty("latitude", listData[listData.size - 1].latitude)
+                dataPredict.addProperty("longitude", listData[listData.size - 1].longitude)
+                dataPredict.add("data", data)
+                homeViewModel.foreCast("userTest", dataPredict)
+                lifecycleScope.launch {
+                    delay(2000)
+                    data = JsonArray()
+                    dataPredict = JsonObject()
+                }
+                homeViewModel.deleteData()
+            }
+        }
+    }
 
     override fun onResume() {
         super.onResume()
 
         getCurrentLocation()
+        createLocationRequest()
+        createLocationCallBack()
+        startLocationUpdates()
     }
 
     override fun onDestroyView() {
@@ -88,29 +147,35 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
+    private fun obtainViewModel(activity: FragmentActivity): HomeViewModel {
+        val factory = RoomViewModelFactory.getInstance(activity.application)
+
+        return ViewModelProvider(activity, factory).get(HomeViewModel::class.java)
+    }
 
     private val requestPermissionLauncher =
         registerForActivityResult(
             ActivityResultContracts.RequestPermission()
-        ){ isGranted ->
-            if (isGranted){
+        ) { isGranted ->
+            if (isGranted) {
                 getCurrentLocation()
             }
         }
 
 
-    private fun getCurrentLocation(){
-        if (ContextCompat.checkSelfPermission(requireContext(),  android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED){
+    private fun getCurrentLocation() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
             fusedLocationClient.lastLocation.addOnSuccessListener { currentLocation ->
-                if (currentLocation != null){
+                if (currentLocation != null) {
                     val lat = currentLocation.latitude
                     val long = currentLocation.longitude
-                    //Log.e("getCurrentLocation: ", " lat = $lat long = $long" )
-                    //PERBAIKI KETIKA LATLONG == NULL
                     getCityName(lat, long)
                 } else {
-                    Toast.makeText(requireContext(), "Turn on your location!!", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(requireContext(), "Nyalakan location!!", Toast.LENGTH_SHORT)
+                        .show()
                 }
             }
         } else {
@@ -122,31 +187,7 @@ class HomeFragment : Fragment() {
     private fun getCityName(lat: Double, long: Double) {
         val geoCoder = Geocoder(requireContext().applicationContext, Locale.getDefault())
         val address = geoCoder.getFromLocation(lat, long, 1)
-        val cityName = address[0].locality
-
-        //cityName.replace("Kecamatan",".")
-        binding.tvAreaName.text = cityName
-        //Log.e("getCityName: ", cityName)
-    }
-
-
-    private fun setupTipsInfo(){
-        listInfoTips = arrayListOf()
-        listInfoTips.addAll(InfoTipsData.listData)
-        tipsInfoAdapter = TipsInfoAdapter(listInfoTips)
-
-        binding.rvTips.apply {
-            adapter = tipsInfoAdapter
-            layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.HORIZONTAL, false)
-        }
-
-        tipsInfoAdapter.setOnItemClickCallback(object : TipsInfoAdapter.OnItemClickCallback{
-            override fun onItemClicked(infoTips: InfoTips) {
-                val intent = Intent(requireContext(), DetailTipsActivity::class.java)
-                intent.putExtra(DetailTipsActivity.EXTRA_TIPS, infoTips)
-                startActivity(intent)
-            }
-        })
+        val cityName: String = address[0].locality
     }
 
 
@@ -157,35 +198,33 @@ class HomeFragment : Fragment() {
         //setAdapter
         binding.rvArea.apply {
             adapter = placeInfoAdapter
-            layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL, false)
+            layoutManager =
+                LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL, false)
         }
 
         //Klik
-        placeInfoAdapter.setOnItemClickCallback(object: PlaceInfoAdapter.OnItemClickCallback{
+        placeInfoAdapter.setOnItemClickCallback(object : PlaceInfoAdapter.OnItemClickCallback {
             override fun onItemClicked(statistic: Statistic) {
                 val intent = Intent(requireContext(), DetailPlaceActivity::class.java)
                 intent.putExtra(DetailPlaceActivity.EXTRA_STATS, statistic)
-                //Log.e("onItemClicked: ", statistic.crime_info.toString())
                 startActivity(intent)
             }
         })
-
         eventChangeListener()
     }
 
 
     private fun eventChangeListener() {
         db = FirebaseFirestore.getInstance()
-        db.collection("statistic").addSnapshotListener(object : EventListener<QuerySnapshot>{
+        db.collection("statistic").addSnapshotListener(object : EventListener<QuerySnapshot> {
             override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
-                if (error != null){
+                if (error != null) {
                     Log.e("Firestore error", error.message.toString())
                     return
                 }
-                for (dc: DocumentChange in value?.documentChanges!!){
-                    if (dc.type == DocumentChange.Type.ADDED){
+                for (dc: DocumentChange in value?.documentChanges!!) {
+                    if (dc.type == DocumentChange.Type.ADDED) {
                         placeInfoData.add(dc.document.toObject(Statistic::class.java))
-                        //Log.e("get data", value.toString())
                     }
                 }
                 placeInfoAdapter.notifyDataSetChanged()
@@ -193,27 +232,84 @@ class HomeFragment : Fragment() {
         })
     }
 
-
-    private fun setupBanner() {
-        val banner = binding.bannerArea
-        banner.setCardBackgroundColor(Color.parseColor("#9AEFD3"))
-
-        //binding.tvAreaName.text = cityName
-        //val description = binding.tvDescArea
-        //val image = binding.ivSafe
-    }
-
-
-    /*private fun setupViewModel() {
-        homeViewModel = ViewModelProvider(requireActivity()).get(HomeViewModel::class.java)
-    }
-
-    private fun setupObserver() {
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            //binding.textHome.text = it
+    private val resolutionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.StartIntentSenderForResult()
+        ) { result ->
+            when (result.resultCode) {
+                AppCompatActivity.RESULT_OK ->
+                    Log.i(
+                        MakeRouteActivity::class.java.simpleName,
+                        "onActivityResult: All location settings are satisfied."
+                    )
+                AppCompatActivity.RESULT_CANCELED ->
+                    Toast.makeText(
+                        requireContext(),
+                        "Anda harus mengaktifkan GPS untuk menggunakan aplikasi ini!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+            }
         }
-    }*/
 
+
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest.create()
+        locationRequest.interval = 120000
+        locationRequest.fastestInterval = 120000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+        val client = LocationServices.getSettingsClient(requireContext())
+        client.checkLocationSettings(builder.build())
+            .addOnSuccessListener { getCurrentLocation() }.addOnFailureListener { exception ->
+                if (exception is ResolvableApiException) {
+                    try {
+                        resolutionLauncher.launch(
+                            IntentSenderRequest.Builder(exception.resolution).build()
+                        )
+                    } catch (sendEx: IntentSender.SendIntentException) {
+                        Toast.makeText(requireContext(), sendEx.message, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }
+    }
+
+    private fun createLocationCallBack() {
+        val dataPredict = DataPredict()
+        val dataTraining = DataTraining()
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                for (location in locationResult.locations) {
+                    val date = DateUtils.getCurrentDate()
+                    dataPredict.let {
+                        dataPredict.latitude = location.latitude
+                        dataPredict.longitude = location.longitude
+                        dataPredict.date = date
+                    }
+                    dataTraining.let {
+                        dataTraining.latitude = location.latitude
+                        dataTraining.longitude = location.longitude
+                        dataTraining.date = date
+                    }
+                    homeViewModel.insertData(dataPredict)
+                    homeViewModel.insertDataTraining(dataTraining)
+                }
+            }
+        }
+    }
+
+    private fun startLocationUpdates() {
+        try {
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        } catch (exception: SecurityException) {
+            Log.e(MakeRouteActivity::class.java.simpleName, "Error : " + exception.message)
+        }
+    }
 
 
     companion object {
