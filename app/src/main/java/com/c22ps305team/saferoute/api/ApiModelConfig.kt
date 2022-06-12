234559f37507ed2fd6e1bd1a59a69a3a10860a77
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-06-12 18:39:55 +0700

    make api config for endpoint model

---
+++ app/src/main/java/com/c22ps305team/saferoute/api/ApiModelConfig.kt
@@ -0,0 +1,29 @@
+package com.c22ps305team.saferoute.api
+
+import com.c22ps305team.saferoute.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiModelConfig {
+    companion object {
+        fun getApiService(): ApiService {
+            val loggingInterceptor = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
+
+            val client = OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+            val retrofit =
+                Retrofit.Builder()
+                    .baseUrl("https://model-ck44nnq7hq-as.a.run.app/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+}
